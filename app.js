21a33fb5e2c3403d1addbbdd6cd665f515fea4a0
Noah Makau <noshmak@gmail.com>
2020-07-02 20:07:18 +0300

    add view engine ejs

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const express = require('express')
 //initialize express
 const app = express()
 
+//setup view engine
+app.set('view engine', 'ejs')
+
 //Set a static folder
 app.use(express.static('public'))
 
+//request route
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
 //Define the port number
-const PORT = 8000
+const PORT = 7000
 
 app.listen(PORT, ()=> {
     console.log(`Server is listening on port ${PORT}`)
